c0fee1578486d5f3db7733518023a8be91d2b6eb
bhavya625 <gadde.bhavya@gmail.com>
2019-06-28 22:55:40 -0400

    Add TextView to display error message

--- app/src/main/java/com/example/arinspectexercise/MainActivity.kt
+++ app/src/main/java/com/example/arinspectexercise/MainActivity.kt
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.TextView
 import com.example.arinspectexercise.model.network.Status
 import com.example.arinspectexercise.viewmodel.FactsViewModel
 
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private var factsAdapter: FactsAdapter = FactsAdapter()
     private lateinit var recyclerView: RecyclerView
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private lateinit var errorMessage: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,8 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.recycler_view)
         swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout)
+        errorMessage = findViewById(R.id.error_message)
+
         recyclerView.adapter = factsAdapter
 
         viewModel = ViewModelProviders.of(this).get(FactsViewModel::class.java)
@@ -34,8 +39,19 @@ class MainActivity : AppCompatActivity() {
         swipeRefreshLayout.isRefreshing = true
         viewModel.getFacts().observe(this, Observer {
             it?.let {
+                //Set loading state
                 swipeRefreshLayout.isRefreshing = it.status == Status.LOADING
+
+                //Set error message on failure and hide the view if message is null or empty
+                if (it.data?.rows.isNullOrEmpty()) {
+                    errorMessage.text = it.message
+                    errorMessage.visibility = if (it.message.isNullOrEmpty()) View.GONE else View.VISIBLE
+                }
+
+                //Set ActionBar title
                 title = it.data?.title
+
+                //Set recyclerview adapter
                 factsAdapter.setData(it.data?.rows)
             }
         })
